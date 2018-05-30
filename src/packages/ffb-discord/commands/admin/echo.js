bd17a3f6148cf9573cefef991f5e60ba590f17f9
Chris <chris062689@gmail.com>
2018-05-30 19:35:22 -0400

    Added echo command.

---
+++ src/packages/ffb-discord/commands/admin/echo.js
@@ -0,0 +1,29 @@
+const logger = require('ffb-logging')
+const ffb = require('ffb-common')
+const commando = require('discord.js-commando');
+
+module.exports = class UserInfoCommand extends commando.Command {
+	constructor(client) {
+		super(client, {
+			name: 'echo',
+			group: 'admin',
+			memberName: 'echo',
+			description: 'Makes the bot echo.',
+            guildOnly: true,
+            ownerOnly: true,
+			args: [
+				{
+					key: 'message',
+					label: 'Message',
+					prompt: `What message do you want to echo?`,
+					type: 'string'
+				}
+			]
+		});
+	}
+
+	async run(message, args) {
+        await message.channel.send(args.messsage)
+        return message.delete()
+	}
+};
